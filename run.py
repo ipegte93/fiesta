4db92573d319a72a9a248f7eb5737f5df5eabbd0
ipegte93 <ipegte93@gmail.com>
2023-03-16 16:33:54 +0900

    Add run

---
+++ run.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+
+# from kivy.uix.label import Label
+# from kivy.uix.gridlayout import GridLayout
+from src.gui.fretboard import FretBoard
+
+
+class MyApp(App):
+    pass
+
+
+if __name__ == "__main__":
+    MyApp().run()
