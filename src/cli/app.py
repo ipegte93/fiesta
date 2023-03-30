7b143f8074884b5f2d4ca15b77bb76ba32b951d6
ipegte93 <ipegte93@gmail.com>
2023-03-30 14:42:19 +0900

    Add Shortcut

--- src/cli/app.py
+++ src/cli/app.py
@@ -2,8 +2,16 @@ from textual.app import App as Textual
 from textual.app import ComposeResult
 
 from src.cli.fretboard import Fretboard
+from src.cli.fretboard._fret import Fret
 
 
 class App(Textual):
+
+    BINDINGS = [("c", "fret_clear", "clear fret")]
+
+    def action_fret_clear(self) -> None:
+        for f in self.query("Fret.-toggle").results(Fret):
+            f.remove_class("-toggle")
+
     def compose(self) -> ComposeResult:
         yield Fretboard()
