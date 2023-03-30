ac56df9c3d1d6dc0242194f5c65ddffd5422f8ab
ipegte93 <ipegte93@gmail.com>
2023-03-31 01:31:12 +0900

    Fix

--- src/note.py
+++ src/note.py
@@ -1,92 +1,76 @@
 from __future__ import annotations
 
 import re
+from collections import deque
+import copy
+
+
+# Alteration
+SHARP = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+FLAT = ('C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B')
 
 
 class Note():
-    # Alteration
-    Sharp = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
-    Flat = ('C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B')
-
-    OCTAVE: bool = False
 
     def __init__(
-        self, note: str | int,
-        alteration: str = "Flat",
-        show_octave: bool = False
+            self,
+            note: str,
+            alteration: str = None,
     ) -> None:
         """
-        alteration : `Sharp`, `Flat`\n
-        Sharp -> C, C#, D, D#, ...\n
-        Flat -> C, Db, D, Eb, ...
+        alteration:
+            sharp: C, C#, D, D#, ...
+            flat: C, Db, D, Eb, ...
         """
-        self.SHOW_OCTAVE = show_octave
 
-        if alteration == "Sharp" or alteration == "Flat":
-            self.ALTERATION = alteration
-        else:
-            raise ValueError("alteration")
+        if type(alteration) is str:
+            alteration = alteration.lower()
 
-        # note is int
-        if type(note) is int:
-            if note > 11:
-                self.OCTAVE = True
-            self.note: int = note
-        # note is str
-        elif type(note) is str:
-            search = re.search(r"^([a-gA-G])(#|b)?([0-9])?", note)
-            # group 1
-            self.note: int = getattr(self, self.ALTERATION).index(
-                search.group(1).upper())
-            # group 2
-            if search.group(2) == "b":
-                self.note -= 1
-            elif search.group(2) == "#":
-                self.note += 1
-            # group 3
-            if search.group(3) is not None:
-                self.note += int(search.group(3)) * 12
-        # note is neither
-        else:
-            raise TypeError
+        if alteration not in {"sharp", "flat", None}:  # from parameter `alteration`
+            raise ValueError("parameter alteration")
+        notes = {"sharp": SHARP, "flat": FLAT}.get(alteration, None)
+
+        if not notes:  # from parameter `note`
+            search = re.search(r"^[a-gA-G](#|b)?", note)
+            notes = {"b": FLAT, "#": SHARP}.get(search.group())
+
+        if not notes:  # if can't get from above
+            notes = SHARP
+
+        self.notes = notes
+        self.note = note
+
+    def rotate(self, num: int) -> None:
+        notes = self.notes
+        notes = deque(notes)
+        notes.rotate(num)
+        self.notes = tuple(notes)
+
+    def interval(self, note: Note | str) -> int:
+        search = re.search(r"^([a-gA-G])(#|b)?", str(note))
+        interval = self.notes.index(search.group(1).upper())
+        interval += {"b": -1, "#": 1}.get(search.group(2), 0)
+        return interval % 12
 
     @property
-    def note(self) -> int:
-        return self.__note
+    def note(self) -> str:
+        return self.notes[0]
 
     @note.setter
-    def note(self, note: int) -> None:
-        if self.OCTAVE:
-            self.__note = note
-        else:
-            self.__note = note % 12
+    def note(self, note: str) -> None:
+        rotate = self.interval(note) * -1
+        self.rotate(rotate)
 
     def __str__(self) -> str:
-        ret = getattr(self, self.ALTERATION)[self.note % 12]
-        if self.SHOW_OCTAVE and self.OCTAVE:
-            ret += str(int(self.note/12)+1)
-        return ret
+        return self.note
 
     def __repr__(self) -> str:
         return f"<Note: {self.__str__()}>"
 
-    def __add__(self, other: int) -> Note:
-        ret = Note(
-            self.note, alteration=self.ALTERATION,
-            show_octave=self.SHOW_OCTAVE
-        )
-        ret.OCTAVE = self.OCTAVE
-        ret.note += other
+    def __add__(self, value: int) -> Note:
+        ret = copy.deepcopy(self)
+        ret.rotate(value * -1)
         return ret
 
-    def __sub__(self, other: int | Note):
-        if type(other) is int:
-            ret = Note(
-                self.note, alteration=self.ALTERATION,
-                show_octave=self.SHOW_OCTAVE
-            )
-            ret.OCTAVE = self.OCTAVE
-            ret.note -= other
-            return ret
-        elif type(other) is Note:
-            return abs(self.note - other.note)
+    def __sub__(self, value: Note | str):
+        return self.interval(value)
