dd7049eefdf30f8919dab63f8349985dfc229df1
ipegte93 <71699228+ipegte93@users.noreply.github.com>
2023-04-06 17:29:24 +0900

    Update _fretboard.py

--- src/tui/fretboard/_fretboard.py
+++ src/tui/fretboard/_fretboard.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
 import re
 
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.widgets import Static
+from textual.message import Message
+from textual.widget import events
 
 from src import Guitar
 from src.calc import interval
@@ -74,3 +77,50 @@ class Horizontal(Widget):
             elif fret.has_class("-toggle"):
                 fret.remove_class("-toggle")
                 fret.border_title = ""
+
+class Fret(Static):
+    DEFAULT_CSS = """
+    Fret {
+        width: 1fr;
+        min-width: 2;
+        height: 3;
+        content-align-horizontal: center;
+        content-align-vertical: middle;
+        border: solid white;
+        border-title-align: center;
+        border-subtitle-align: center;
+        color: $surface;
+    }
+    Fret:hover {
+        color: $text;
+    }
+    Fret:disabled {
+        border: solid $background-lighten-3;
+        color: $surface;
+    }
+    Fret.-toggle {
+        border: solid gold;
+        color: gold;
+    }
+    """
+
+    class Pressed(Message, bubble=True):
+        """Fret Pressed Message."""
+
+        def __init__(self, fret: Fret) -> None:
+            self.fret = fret
+            super().__init__()
+
+        @property
+        def control(self) -> Fret:
+            return self.fret
+
+    async def _on_click(self, event: events.Click) -> None:
+        event.stop()
+
+        self.toggle_class("-toggle")
+        if not self.has_class("-toggle"):
+            self.border_title = ""
+
+        self.post_message(Fret.Pressed(self))
+
